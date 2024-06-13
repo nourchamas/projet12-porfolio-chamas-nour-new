a485f5293864c5e5aade26759dcd0932f9c2ecc3
nourchamas <153526452+nourchamas@users.noreply.github.com>
2024-06-13 17:03:30 +0200

    changements

--- src/data/info.ts
+++ src/data/info.ts
@@ -1,90 +1,71 @@
 export const info = {
-  name: "Chamas nour",
+  name: "Chamas Nour",
   brief_description:
     "une développeuse front-end passionnée",
   role: "Développeuse front-end",
   picture: "/public/nour.jpg",
   picture_alt: "Chamas Nour",
   location: "France",
-  cv: "/resume.pdf",
+  cv: "/public/resume chamas nour .pdf",
 
   about: {
-    description: `Bienvenue sur mon portfolio ! Je m'appelle [Votre Nom],
+    description: `Bienvenue sur mon portfolio ! Je m'appelle Nour,
      et je suis une développeuse front-end passionnée ainsi qu'une intégratrice web, 
      formée par OpenClassrooms. Ce portfolio présente mes compétences, mes projets et mon parcours professionnel. Mon objectif est de créer des expériences utilisateur intuitives et visuellement attrayantes tout en assurant des sites web performants et accessibles.`,
-    education: [
+   
+      
+       
+  
+    
+    Experience: [
+
       {
-        title: "Self-Taught Developer Extraordinaire",
-        date: "2000 - Present",
-        location: "Springfield Elementary School of Hard Knocks",
-        gpa: "4.0 (A+ in Pranks)",
-        thesis: "The Art of Subverting Expectations: A Practical Guide",
-      },
-    ],
-    experience: [
-      {
-        title: "Frontend Developer",
-        date: "2015 - Present",
-        location: "Krusty Burger Web Dev",
+        title: "openclassroom",
+        date: "2023",
         description:
-          "Crafted interactive web experiences using React and Redux. Often multitasked between coding and avoiding Principal Skinner.",
-      },
-      {
-        title: "Backend Bandit",
-        date: "2013 - 2015",
-        location: "Comic Book Guy's Software Emporium",
-        description:
-          "Masterminded backend solutions using Node.js and MongoDB. Surprisingly good at debugging, considering my history of causing bugs.",
-      },
-      {
-        title: "Junior Hacker",
-        date: "2010 - 2013",
-        location: "Springfield Elementary Cyber Club",
-        description:
-          "Pioneered early web exploits, leading the club in daring coding escapades. Was disciplined for hacking the school's grading system.",
+          "front-end /integrateur web "
       },
     ],
 
-    skills: ["JavaScript", "React", "Node.js", "MongoDB", "Prank Mastery"], // not used yet
+    skills: ["Html","Css","JavaScript", "React", "Node.js",], 
   },
 
   projects: [
     {
-      title: "Bart's Prank Portfolio",
+      title: " Portfolio",
       date: "2024",
       description:
         "An interactive portfolio website showcasing my mischievous coding projects.",
-      link: "https://github.com/bartsimpsonfake/prank-portfolio",
-      tech: ["React", "Node.js", "MongoDB", "Sarcasm.js"],
-      img_alt: "Bart's Portfolio",
-      img_path: "/simpsons.png",
+      
+      tech: ["React", "Node.js",],
+      img_alt: "portfolio",
+      img_path: ""
     },
     {
-      title: "Skateboard Shop",
+      title: "Debuggez le site ",
       date: "2023",
       description:
-        "An e-commerce site selling skateboards and prank supplies. No bullies allowed!",
-      link: "https://github.com/bartsimpsonfake/skate-shop",
-      tech: ["React", "Node.js", "MongoDB", "Skateboard.js"],
-      img_alt: "Skateboard Shop",
-      img_path: "skateboard.png",
+        ":Débugger un site web 724 events.",
+      link: "https://github.com/nourchamas/Debuggez-le-site_projet10.git",
+      tech: ["React", "test unitaires"],
+      img_alt: "724events",
+      img_path: "/public/724 events.png",
     },
     {
-      title: "Comic Book Club",
-      date: "2022",
+      title: "projet 3 Booki",
+      date: "2023",
       description:
-        "A platform for comic book enthusiasts to discuss and share their collections.",
-      link: "https://github.com/bartsimpsonfake/comic-club",
-      tech: ["React", "Node.js", "MongoDB", "Comic.js"],
-      img_alt: "Comic Book Club",
-      img_path: "comics.png",
+        "Description :Créez la page d'accueil d'une agence de voyage avec HTML & css",
+      link: "https://github.com/nourchamas/Booki.git",
+      tech: ["Html", "css",],
+      img_alt: "Booki",
+      img_path: "/public/booki projet.png",
     },
   ],
 
   contact: {
-    email: "bart@thesimpsons.com",
-    linkedin: "https://www.linkedin.com/in/bartsimpsonfake",
-    github: "https://www.github.com/gio-del",
-    twitter: "https://www.twitter.com/bartsimpsonfake",
+    email: "nour.chamas@outlook.com",
+    github: "https://github.com/nourchamas",
+   
   },
 };
