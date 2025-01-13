96f4b63e6f70dc45e2373c3e5735fc74a06ce34a
nourchamas <153526452+nourchamas@users.noreply.github.com>
2025-01-13 16:24:37 +0200

    Update info.ts
    supprimer l'expérience des données

--- src/data/info.ts
+++ src/data/info.ts
@@ -1,85 +1,68 @@
-import Education from "../components/about_section/Education";
-
 export const info = {
-  name:" Nour Chamas",
-  brief_description:
-    "une développeuse front-end passionnée",
+  name: " Nour Chamas",
+  brief_description: "une développeuse front-end passionnée",
   role: "Développeuse front-end",
   picture: "/nour photo.jpg",
   picture_alt: "Chamas Nour",
   location: "France",
   cv: "/resume chamas nour .pdf",
 
-   
-     about: {
-      description: `Bienvenue sur mon portfolio ! Je m'appelle Nour,
+  about: {
+    description: `Bienvenue sur mon portfolio ! Je m'appelle Nour,
       et je suis une développeuse front-end passionnée ainsi qu'une intégratrice web, 
       formée par OpenClassrooms. Ce portfolio présente mes compétences, mes projets et mon parcours professionnel. Mon objectif est de créer des expériences utilisateur intuitives et visuellement attrayantes tout en assurant des sites web performants et accessibles.`,
-      education: [
-        {
-          title: "formation integrateur web /front-end",
-          date: "2023-2024",
-          location: "openclassroom a distance",
-          
-        }
-      ],
-
-      experience: [{}, {
-
-      }, {
-      
-      }],
-     
-    skills: ["Html","Css","JavaScript", "React", "Node.js",], 
+    education: [
+      {
+        title: "formation integrateur web /front-end",
+        date: "2023-2024",
+        location: "openclassroom a distance",
+      },
+    ],
+    experience: [],
+    skills: ["Html", "Css", "JavaScript", "React", "Node.js"],
   },
-  
+
   projects: [
     {
       title: " Portfolio",
       date: "2024",
-      description:
-        "An interactive portfolio website showcasing my mischievous coding projects.",
-      
-      tech: ["React", "Node.js",],
+      description: "An interactive portfolio website showcasing my mischievous coding projects.",
+
+      tech: ["React", "Node.js"],
       img_alt: "portfolio",
-      img_path: "/public/screenshot nour.png",
+      img_path: "/screenshot nour.png",
     },
-    { 
+    {
       title: "Debuggez le site ",
       date: "2023",
-      description:
-        ":Débugger un site web 724 events.",
+      description: ":Débugger un site web 724 events.",
       link: "https://github.com/nourchamas/Debuggez-le-site_projet10.git",
       tech: ["React", "test unitaires"],
       img_alt: "724events",
-      img_path:"/public/724 events .png"
+      img_path: "/724 events .png",
     },
     {
       title: "Booki",
       date: "2023",
-      description:
-        "Description :Créez la page d'accueil d'une agence de voyage avec HTML & css",
+      description: "Description :Créez la page d'accueil d'une agence de voyage avec HTML & css",
       link: "https://github.com/nourchamas/Booki.git",
       tech: ["Html", "css"],
       img_alt: "Booki",
-      img_path: "/public/booki projet.png",
+      img_path: "/booki projet.png",
     },
     {
-       title:"Kaza",
-       date: "2024",
-       description:
-         "Créez une application web de location immobilière avec React",
-         link:"https://github.com/nourchamas/kasa.git",
-         tech:["Html,Css,React,Javascript"],
-         img_alt: "Kaza photo",
-         img_path:"/public/kasa photo.png",
+      title: "Kaza",
+      date: "2024",
+      description: "Créez une application web de location immobilière avec React",
+      link: "https://github.com/nourchamas/kasa.git",
+      tech: ["Html,Css,React,Javascript"],
+      img_alt: "Kaza photo",
+      img_path: "/kasa photo.png",
     },
   ],
-
 
   contact: {
     email: "nour.chamas@outlook.com",
     github: "https://github.com/nourchamas",
-   
   },
 };
